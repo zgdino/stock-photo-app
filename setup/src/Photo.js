cae806a7dba0a811bf786d83cb342309e142dc9a
Dino Hadzic <zgdino@yahoo.com>
2022-05-09 16:27:01 +0200

    photo component

--- setup/src/Photo.js
+++ setup/src/Photo.js
@@ -1,7 +1,24 @@
 import React from 'react'
 
-const Photo = () => {
-  return <h2>photo component</h2>
+const Photo = ({
+  urls: { regular },
+  alt_description,
+  likes,
+  user: {
+    name,
+    portfolio_url,
+    profile_image: { medium },
+  },
+}) => {
+  return (
+    <article className='photo'>
+      <img src={regular} alt={alt_description} />
+      <div className='photo-info'>
+        <h4>{name}</h4>
+        <p>{likes} likes</p>
+      </div>
+    </article>
+  )
 }
 
 export default Photo
