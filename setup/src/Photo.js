2df97833c29bbbaa1b9f05567aba2160024c6d0e
Dino Hadzic <zgdino@yahoo.com>
2022-05-09 16:35:41 +0200

    photo component

--- setup/src/Photo.js
+++ setup/src/Photo.js
@@ -14,8 +14,13 @@ const Photo = ({
     <article className='photo'>
       <img src={regular} alt={alt_description} />
       <div className='photo-info'>
-        <h4>{name}</h4>
-        <p>{likes} likes</p>
+        <div>
+          <h4>{name}</h4>
+          <p>{likes} likes</p>
+        </div>
+        <a href={portfolio_url}>
+          <img src={medium} alt={name} className='user-img' />
+        </a>
       </div>
     </article>
   )
