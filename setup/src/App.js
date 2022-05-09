242452f5df0dbbdce6e2da90fdf4bedf17816339
Dino Hadzic <zgdino@yahoo.com>
2022-05-09 16:09:32 +0200

    loading at the bottom

--- setup/src/App.js
+++ setup/src/App.js
@@ -51,6 +51,7 @@ function App() {
             return <Photo key={index} {...image} />
           })}
         </div>
+        {loading && <h2 className='loading'>Loading...</h2>}
       </section>
     </main>
   )
